3a96d9791283b629ad7e0438758bba3bec2b35a2
CreidenCR99 <adriancuervorodriguez@gmail.com>
2026-01-29 14:22:44 +0100

    Actualización stock y nuevoPedido (29/01/26)

---
+++ src/main/java/es/educastur/adriancr37/practicatienda/StockInsuficiente.java
@@ -0,0 +1,8 @@
+package es.educastur.adriancr37.practicatienda;
+
+public class StockInsuficiente extends Exception {
+    public StockInsuficiente (String mensaje) {
+        super(mensaje);
+    }
+}
+
