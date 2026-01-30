4b1f4a45dd4028164e9040a1e4c323f84e89989d
CreidenCR99 <adriancuervorodriguez@gmail.com>
2026-01-30 12:00:31 +0100

    Actualizacion totalPedido (30/01/26 - 2)

--- src/main/java/es/educastur/adriancr37/practicatienda/StockInsuficiente.java
+++ src/main/java/es/educastur/adriancr37/practicatienda/StockInsuficiente.java
@@ -4,9 +4,4 @@ public class StockInsuficiente extends Exception {
     public StockInsuficiente (String mensaje) {
         super(mensaje);
     }
-<<<<<<< HEAD
 }
-=======
-}
-
->>>>>>> 3c19c37dacfe9ea1d3c7da76d536bcc662e50576
